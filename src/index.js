4a4cc58f9db9e24227d20b64b6c6c5a0097e6907
Worameth Semapat <worameth.semapat@gmail.com>
2018-02-15 10:53:46 +0700

    add react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div className="App">
+      <h1 className="App-Title">Hello Parcel x React</h1>
+    </div>
+  );
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
